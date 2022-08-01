3b900a32bc1e45794a2de93824e5cb8bca1a2ee0
Esmond Wong <hello@camerash.com>
2022-08-01 18:35:24 +0800

    Update configs

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter_scaffold/features/home/view/home.dart';
+import 'package:flutter_scaffold/features/home/home.dart';
 import 'package:flutter_scaffold/features/login/login.dart';
 
 @AdaptiveAutoRouter(
--- lib/core/navigation/app_router.gr.dart
+++ lib/core/navigation/app_router.gr.dart
@@ -12,7 +12,7 @@
 
 import 'package:auto_route/auto_route.dart' as _i3;
 import 'package:flutter/material.dart' as _i4;
-import 'package:flutter_scaffold/features/home/view/home.dart' as _i2;
+import 'package:flutter_scaffold/features/home/home.dart' as _i2;
 import 'package:flutter_scaffold/features/login/login.dart' as _i1;
 
 class AppRouter extends _i3.RootStackRouter {
